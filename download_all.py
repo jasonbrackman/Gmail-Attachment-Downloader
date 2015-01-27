4680c4209f38383c54f11433dad4bfb08d1ac3c2
brackman <brackman@gmail.com>
2015-01-26 21:40:30 -0800

    - integrated some updates from original depot.

--- download_all.py
+++ download_all.py
@@ -4,29 +4,15 @@
 # https://support.google.com/accounts/answer/185833
 
 # imap protocol doc: http://tools.ietf.org/html/rfc3501
+
 import re
 import email
 import hashlib
 import getpass
 import imaplib
 import os
-from collections import defaultdict
+from collections import defaultdict, Counter
 import platform
-
-
-def get_hash(file_to_hash):
-    # return unique hash of file
-    block_size = 65536
-    hasher = hashlib.md5()
-    try:
-        with open(file_to_hash, 'rb') as handle:
-            buf = handle.read(block_size)
-            while len(buf) > 0:
-                hasher.update(buf)
-                buf = handle.read(block_size)
-    except IOError as err:
-        print(err)
-    return hasher.hexdigest()
 
 
 def make_attachment_directory(path):
@@ -92,11 +78,11 @@ def gmail_login(username, password):
 def get_gmail_attachments_for_message_ids(mail, message_IDs, filter_set=None):
 
     # setup caching of names/data gleaned from attachments
-    fileNameCount_dict = defaultdict(int)
-    fileNameList_dict = defaultdict(list)
+    fileNameCounter = Counter()
+    fileNameHashes = defaultdict(set)
 
     # ensure the directory where the files will be stored go into an attachments folder
-    attachment_directory = make_attachment_directory('.')
+    directory = make_attachment_directory('.')
 
     # Fetch the messages w/ attachments and download
     for message_ID in message_IDs:
@@ -116,66 +102,52 @@ def get_gmail_attachments_for_message_ids(mail, message_IDs, filter_set=None):
         message = message_from(emailBody)
         for part in message.walk():
             if part.get_content_maintype() == 'multipart':
-                # print part.as_string()
                 continue
             if part.get('Content-Disposition') is None:
-                # print part.as_string()
                 continue
+
             filename = part.get_filename()
 
             if filename is not None:
                 if filter_set is not None and not filename.lower().endswith(filter_set):
-                    print(filename)
                     continue
                 filename = ''.join(x for x in filename.splitlines())
-            if bool(filename):
-                filePath = os.path.join(attachment_directory, 'temp.attachment')
-                if os.path.isfile(filePath):
-                    os.remove(filePath)
-                if not os.path.isfile(filePath):
-                    try:
-                        # print 'Processing: {file}'.format(file=fileName)
-                        fp = open(filePath, 'wb')
-                        fp.write(part.get_payload(decode=True))
-                        fp.close()
-                        x_hash = get_hash(filePath)
-                    except Exception as e:
-                        print(e)
-                        x_hash = get_hash(filePath)
 
-                    if x_hash in fileNameList_dict[filename]:
+            if filename:
+                payload = part.get_payload(decode=True)
+                if payload:
+                    x_hash = hashlib.md5(payload).hexdigest()
+                    if x_hash in fileNameHashes[filename]:
                         print('\tSkipping duplicate file: {file}'.format(file=filename))
-
+                        continue
+                    fileNameCounter[filename] += 1
+                    fileStr, fileExtension = os.path.splitext(filename)
+                    if fileNameCounter[filename] > 1:
+                        new_fileName = '{file}({suffix}){ext}'.format(suffix=fileNameCounter[filename],
+                                                                      file=fileStr,
+                                                                      ext=fileExtension)
+                        # print('\tRenaming and storing: {file} to {new_file}'.format(file=filename,
+                                                                                    # new_file=new_fileName))
                     else:
-                        fileNameCount_dict[filename] += 1
-                        fileStr, fileExtension = os.path.splitext(filename)
-                        if fileNameCount_dict[filename] > 1:
-                            new_fileName = '{file}({suffix}){ext}'.format(suffix=fileNameCount_dict[filename],
-                                                                          file=fileStr, ext=fileExtension)
-                        else:
-                            new_fileName = filename
-                        fileNameList_dict[filename].append(x_hash)
-                        hash_path = os.path.join(attachment_directory, new_fileName)
-                        if not os.path.isfile(hash_path):
-                            if new_fileName == filename:
-                                print('\tStoring: {file}'.format(file=filename))
-                            else:
-                                print('\tRenaming and storing: {file} to {new_file}'.format(file=filename,
-                                                                                            new_file=new_fileName))
-                            try:
-                                os.rename(filePath, hash_path)
-                            except Exception as e:
-                                print(type(e))
-                                print(
-                                    'Could not store: {file} (invalid filename or path under {op_sys}.'.format(
-                                        file=hash_path, op_sys=platform.system()))
-                        elif os.path.isfile(hash_path):
-                            print('\tExists in destination: {file}'.format(file=new_fileName))
-                    if os.path.isfile(filePath):
-                        os.remove(filePath)
-
-    # mail.close()
-    # mail.logout()
+                        new_fileName = filename
+                        # print('\tStoring: {file}'.format(file=filename))
+                    fileNameHashes[filename].add(x_hash)
+                    file_path = os.path.join(directory, new_fileName)
+                    if os.path.exists(file_path):
+                        print('\tExists in destination: {file}'.format(file=new_fileName))
+                        continue
+                    try:
+                        with open(file_path, 'wb') as fp:
+                            fp.write(payload)
+                    except Exception as e:
+                        print(type(e))
+                        print('Could not store: {file} (invalid name or path under {op_sys}.'.format(
+                            file=file_path,
+                            op_sys=platform.system()))
+                else:
+                    print('Attachment {file} was returned as type: {ftype} skipping...'.format(file=filename,
+                                                                                               ftype=type(payload)))
+                    continue
 
 if __name__ == "__main__":
 
@@ -195,3 +167,7 @@ if __name__ == "__main__":
 
     # filter out files of a specific type
     get_gmail_attachments_for_message_ids(mail, message_IDs, filter_set=filters)
+
+    # close up shop
+    mail.close()
+    mail.logout()
